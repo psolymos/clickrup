a9e4c9620be52727e3916f69b77bfe95a389c261
Peter Solymos <psolymos@gmail.com>
2020-06-22 13:32:51 -0600

    Create cu-task-id.R

---
+++ R/cu-task-id.R
@@ -0,0 +1,23 @@
+#' Handling task IDs.
+#'
+#' Task IDs are prepended with a hash when copied from the ClickUp GUI.
+#' The function checks and removes the leading hash.
+
+#' @param task_id Task ID with or without a leading hash.
+#'
+#' @examples
+#'
+#' cu_task_id("#7ygh8h")
+#' cu_task_id("7ygh8h")
+#'
+#' @return
+#'
+#' Returns task ID as character without leading hash to be used in the API.
+#'
+#' @export
+#' @name cu-task-id
+
+## this allows manipulating cu options conveniently
+cu_task_id <- function(task_id) {
+    gsub("#", "", as.character(task_id))
+}
