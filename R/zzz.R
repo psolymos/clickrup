8e48250e2922453c763909792ad707d070c76fe3
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-02 09:19:29 +0200

    Only reset options when they were empty initially

--- R/zzz.R
+++ R/zzz.R
@@ -5,18 +5,24 @@
     invisible(NULL)
 }
 
+options_set <- FALSE
+
 .onLoad <- function(libname, pkgname) {
-    if (is.null(getOption("cu_options")))
+    if (is.null(getOption("cu_options"))) {
+        options_set <<- TRUE
         options("cu_options" = list(
             baseurl = "https://api.clickup.com", # API url
             version = "v2",                      # API version
             tz = "",                             # time zone for unix dates
             useragent = "http://github.com/psolymos/clickrup"
         ))
+    }
     invisible(NULL)
 }
 
 .onUnload <- function(libpath) {
-    options("cu_options" = NULL)
+    if (options_set) {
+        options("cu_options" = NULL)
+    }
     invisible(NULL)
 }
