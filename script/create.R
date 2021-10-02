bd4d70e1562e8ebe68b8a3ae11c1ec5366e66060
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-02 13:23:10 +0200

    Purge

--- script/create.R
+++ script/create.R
@@ -1,7 +1,15 @@
 pkgload::load_all()
+library(tidyverse)
 
-team_id <- cuf_get_teams()$id[[1]]
+# team_id <- cuf_get_teams()$id[[1]]
+team_id <- "24312410"
+
+spaces <- cuf_get_spaces(team_id)
+
+walk(spaces, cu_delete_space)
+
+asdf
 
 cuf_create_space(team_id, "Test space 1")
-space_df_2 <- cuf_create_space(team_id, "Test space 2")
+cuf_create_space(team_id, "Test space 2")
 space_df_2
