313ac336f62b2b27f5af446bcd2efde00243e13e
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-02 09:18:06 +0200

    Use .onLoad() for options

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,17 @@
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                     fields=c("Version", "Date"))
     packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
+    invisible(NULL)
+}
+
+.onLoad <- function(libname, pkgname) {
     if (is.null(getOption("cu_options")))
         options("cu_options" = list(
             baseurl = "https://api.clickup.com", # API url
             version = "v2",                      # API version
             tz = "",                             # time zone for unix dates
             useragent = "http://github.com/psolymos/clickrup"
-            ))
+        ))
     invisible(NULL)
 }
 
