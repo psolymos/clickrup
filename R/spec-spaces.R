2b83750af267b40ff6f071234ca3e8cd086c3371
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-03 05:09:55 +0200

    Refresh spaces

--- R/spec-spaces.R
+++ R/spec-spaces.R
@@ -4,6 +4,7 @@ spec_spaces <- lcols(
     private = lcol_lgl("private"),
     statuses = lcol_df_lst(
         "statuses",
+        id = lcol_chr("id"),
         status = lcol_chr("status"),
         type = lcol_chr("type"),
         orderindex = lcol_int("orderindex"),
@@ -19,45 +20,100 @@ spec_spaces <- lcols(
             remap_due_dates = lcol_lgl("remap_due_dates"),
             remap_closed_due_date = lcol_lgl("remap_closed_due_date")
         ),
+        sprints = lcol_df(
+            "sprints",
+            enabled = lcol_lgl("enabled")
+        ),
         time_tracking = lcol_df(
             "time_tracking",
             enabled = lcol_lgl("enabled"),
-            .default = tibble(enabled = NA)
+            harvest = lcol_lgl("harvest"),
+            rollup = lcol_lgl("rollup")
+        ),
+        points = lcol_df(
+            "points",
+            enabled = lcol_lgl("enabled")
+        ),
+        custom_items = lcol_df(
+            "custom_items",
+            enabled = lcol_lgl("enabled")
+        ),
+        priorities = lcol_df(
+            "priorities",
+            enabled = lcol_lgl("enabled"),
+            priorities = lcol_df_lst(
+                "priorities",
+                id = lcol_chr("id"),
+                priority = lcol_chr("priority"),
+                color = lcol_chr("color"),
+                orderindex = lcol_chr("orderindex")
+            )
         ),
         tags = lcol_df(
             "tags",
-            enabled = lcol_lgl("enabled"),
-            .default = tibble(enabled = NA)
+            enabled = lcol_lgl("enabled")
+        ),
+        wip_limits = lcol_df(
+            "wip_limits",
+            enabled = lcol_lgl("enabled", .default = NA),
+            .default = NULL
         ),
         time_estimates = lcol_df(
             "time_estimates",
-            enabled = lcol_lgl("enabled"),
-            .default = tibble(enabled = NA)
+            enabled = lcol_lgl("enabled", .default = NA),
+            rollup = lcol_lgl("rollup", .default = NA),
+            per_assignee = lcol_lgl("per_assignee", .default = NA),
+            .default = NULL
         ),
-        checklists = lcol_df(
-            "checklists",
+        check_unresolved = lcol_df(
+            "check_unresolved",
             enabled = lcol_lgl("enabled"),
-            .default = tibble(enabled = NA)
+            subtasks = lcol_lgl("subtasks", .default = NA),
+            checklists = lcol_guess("checklists", .default = NULL),
+            comments = lcol_guess("comments", .default = NULL)
+        ),
+        zoom = lcol_df(
+            "zoom",
+            enabled = lcol_lgl("enabled")
+        ),
+        milestones = lcol_df(
+            "milestones",
+            enabled = lcol_lgl("enabled")
         ),
         custom_fields = lcol_df(
             "custom_fields",
-            enabled = lcol_lgl("enabled", .default = NA),
-            .default = tibble(enabled = NA)
+            enabled = lcol_lgl("enabled")
         ),
         remap_dependencies = lcol_df(
             "remap_dependencies",
             enabled = lcol_lgl("enabled", .default = NA),
-            .default = tibble(enabled = NA)
+            .default = NULL
         ),
         dependency_warning = lcol_df(
             "dependency_warning",
-            enabled = lcol_lgl("enabled", .default = NA),
-            .default = tibble(enabled = NA)
+            enabled = lcol_lgl("enabled")
         ),
-        portfolios = lcol_df(
-            "portfolios",
+        multiple_assignees = lcol_df(
+            "multiple_assignees",
             enabled = lcol_lgl("enabled", .default = NA),
-            .default = tibble(enabled = NA)
+            .default = NULL
+        ),
+        emails = lcol_df(
+            "emails",
+            enabled = lcol_lgl("enabled")
         )
+    ),
+    archived = lcol_lgl("archived"),
+    members = lcol_df_lst(
+        "members",
+        user = lcol_df(
+            "user",
+            id = lcol_chr("id", .parser = as.character),
+            username = lcol_chr("username"),
+            color = lcol_chr("color"),
+            profilePicture = lcol_chr("profilePicture"),
+            initials = lcol_chr("initials")
+        ),
+        .default = NULL
     )
 )
