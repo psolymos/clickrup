afa244329a84e0651b364d3e5cfa42f673602a86
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-03 05:30:51 +0200

    Get all folders

--- script/tibblify-03-folders.R
+++ script/tibblify-03-folders.R
@@ -5,10 +5,14 @@ library(tidyverse)
 df_teams <- cuf_get_teams()
 df_spaces <- cuf_get_spaces(df_teams$id[[1]])
 
-folders <- cu_get_folders(df_spaces$id[[1]])
+folders <-
+    df_spaces$id %>%
+    map(cu_get_folders) %>%
+    map("folders") %>%
+    unlist(recursive = FALSE)
 folders
 
-df_folders <- tibblify::tibblify(folders$folders)
+df_folders <- tibblify::tibblify(folders)
 df_folders
 df_folders %>% get_spec()
 
