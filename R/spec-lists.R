9a77e8b05ebd0b6c3a02e63f90615c455a7ce60e
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-03 11:40:32 +0200

    Better specs

--- R/spec-lists.R
+++ R/spec-lists.R
@@ -3,12 +3,24 @@ spec_lists <- lcols(
     name = lcol_chr("name"),
     orderindex = lcol_int("orderindex"),
     content = lcol_chr("content", .default = NA_character_),
-    status = lcol_guess("status", .default = NULL),
+    status = lcol_df(
+        "status",
+        status = lcol_chr("status", .default = NA_character_),
+        color = lcol_chr("color", .default = NA_character_),
+        .default = NULL
+    ),
     priority = lcol_guess("priority", .default = NULL),
-    assignee = lcol_guess("assignee", .default = NULL),
+    assignee = lcol_df(
+        "assignee",
+        color = lcol_chr("color", .default = NA_character_),
+        username = lcol_chr("username", .default = NA_character_),
+        initials = lcol_chr("initials", .default = NA_character_),
+        profilePicture = lcol_chr("profilePicture", .default = NA_character_),
+        .default = NULL
+    ),
     task_count = lcol_int("task_count"),
-    due_date = lcol_guess("due_date", .default = NULL),
-    start_date = lcol_guess("start_date", .default = NULL),
+    due_date = lcol_dtt("due_date", .parser = cu_date_from, .default = dttr2::NA_POSIXct_),
+    start_date = lcol_dtt("start_date", .parser = cu_date_from, .default = dttr2::NA_POSIXct_),
     folder = lcol_df(
         "folder",
         id = lcol_chr("id"),
--- script/tibblify-04-lists.R
+++ script/tibblify-04-lists.R
@@ -5,12 +5,16 @@ library(tidyverse)
 df_teams <- cuf_get_teams()
 df_spaces <- cuf_get_spaces(df_teams$id[[1]])
 
-df_folders <- cuf_get_folders(df_spaces$id[[1]])
+df_folders <- map_dfr(df_spaces$id, cuf_get_folders)
 df_folders
 
-lists <- cu_get_lists(df_folders$id[[1]])
+lists <-
+    df_folders$id %>%
+    map(cu_get_lists) %>%
+    map("lists") %>%
+    unlist(recursive = FALSE)
 
-df_lists <- tibblify::tibblify(lists$lists)
+df_lists <- tibblify::tibblify(lists)
 df_lists
 df_lists %>% get_spec()
 
