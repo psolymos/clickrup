175b08b2fbc8c097f4fb98e6aa278874fa58ebad
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-03 05:08:02 +0200

    Character ID columns

--- R/tibblify.R
+++ R/tibblify.R
@@ -14,7 +14,12 @@ write_spec <- function(x) {
     base_name <- gsub("^df_", "", name)
 
     out <- paste0("spec_", base_name, " <- ", out)
-    out <- gsub("  ", "    ", out)
+    out <- gsub("  ", "    ", out, fixed = TRUE)
+    out <- gsub('lcol_int("id")', 'lcol_chr("id")', out, fixed = TRUE)
+    out <- gsub('lcol_int("id", .default = NA_integer_', 'lcol_chr("id", .default = NA_character_', out, fixed = TRUE)
+
+    # Dogfood first
+    eval(parse(text = out))
 
     writeLines(out, file.path("R", paste0("spec-", base_name, ".R")))
 }
