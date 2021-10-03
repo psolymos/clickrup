94da9513fca7cbb158a22d36b4a718cb69e2094c
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-03 05:31:56 +0200

    Tweak

--- R/df-folders.R
+++ R/df-folders.R
@@ -6,7 +6,7 @@ NULL
 #' @rdname api-folders
 cuf_create_folder <- function(space_id, name) {
     out <- cu_create_folder(space_id, name)
-    tibblify(list(out), spec_folder)
+    tibblify(list(out), spec_folders)
 }
 
 
@@ -14,7 +14,7 @@ cuf_create_folder <- function(space_id, name) {
 #' @rdname api-folders
 cuf_update_folder <- function(folder_id, name) {
     out <- cu_update_folder(folder_id, name)
-    tibblify(list(out), spec_folder)
+    tibblify(list(out), spec_folders)
 }
 
 
@@ -22,7 +22,7 @@ cuf_update_folder <- function(folder_id, name) {
 #' @rdname api-folders
 cuf_get_folders <- function(space_id, archived=FALSE) {
     out <- cu_get_folders(space_id, archived)
-    tibblify(out$folders, spec_folder)
+    tibblify(out$folders, spec_folders)
 }
 
 
@@ -31,5 +31,5 @@ cuf_get_folders <- function(space_id, archived=FALSE) {
 #' @rdname api-folders
 cuf_get_folder <- function(folder_id) {
     out <- cu_get_folder(folder_id)
-    tibblify(list(out), spec_folder)
+    tibblify(list(out), spec_folders)
 }
