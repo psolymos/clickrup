44ccf201675074a08a29fdcbb9c3e1f65067d69d
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-22 17:47:46 +0200

    Use new API for search + replace

--- R/tibblify.R
+++ R/tibblify.R
@@ -16,16 +16,16 @@ write_spec <- function(x) {
     out <- paste0("spec_", base_name, " <- ", out)
     out <- gsub("  ", "    ", out, fixed = TRUE)
 
-    out <- gsub('lcol_int("id")', 'lcol_chr("id", .parser = as.character)', out, fixed = TRUE)
-    out <- gsub('lcol_int("id", .default = NA_integer_', 'lcol_chr("id", .parser = as.character, .default = NA_character_', out, fixed = TRUE)
+    out <- gsub('tib_int("id")', 'tib_chr("id", transform = as.character)', out, fixed = TRUE)
+    out <- gsub('tib_int("id", default = NA_integer_', 'tib_chr("id", transform = as.character, default = NA_character_', out, fixed = TRUE)
 
-    out <- gsub('lcol_chr("orderindex")', 'lcol_int("orderindex", .parser = as.integer)', out, fixed = TRUE)
+    out <- gsub('tib_chr("orderindex")', 'tib_int("orderindex", transform = as.integer)', out, fixed = TRUE)
 
     date_cols <- "last_active|date_joined|date_invited|start_date|end_date|due_date|start|end|date_created|date_updated|date_closed|at"
-    out <- gsub(paste0('lcol_chr[(]"(', date_cols, ')", .default = NA_character_'), 'lcol_dtt("\\1", .parser = cu_date_from, .default = dttr2::NA_POSIXct_', out)
-    out <- gsub(paste0('lcol_chr[(]"(', date_cols, ')"'), 'lcol_dtt("\\1", .parser = cu_date_from', out)
+    out <- gsub(paste0('tib_chr[(]"(', date_cols, ')", default = NA_character_'), 'tib_vector("\\1", ptype = vctrs::new_datetime(), transform = cu_date_from, default = dttr2::NA_POSIXct_', out)
+    out <- gsub(paste0('tib_chr[(]"(', date_cols, ')"'), 'tib_vector("\\1", ptype = vctrs::new_datetime(), transform = cu_date_from', out)
 
-    out <- gsub('lcol_chr("duration", .default = NA_character_)', 'lcol_vec("duration", hms::new_hms(), .default = dttr2::NA_hms_, .parser = ~ hms::as_hms(as.numeric(.x) / 1000))', out, fixed = TRUE)
+    out <- gsub('tib_chr("duration", default = NA_character_)', 'tib_vector("duration", hms::new_hms(), default = dttr2::NA_hms_, transform = ~ hms::as_hms(as.numeric(.x) / 1000))', out, fixed = TRUE)
 
     # Dogfood first
     eval(parse(text = out))
