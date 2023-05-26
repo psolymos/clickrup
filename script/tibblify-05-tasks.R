4c06a1a76d32f39e854d0be84e0fe8b752e503b8
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-26 15:28:56 +0200

    tasks

--- script/tibblify-05-tasks.R
+++ script/tibblify-05-tasks.R
@@ -8,11 +8,11 @@ df_spaces <- cuf_get_spaces(df_teams$id[[1]])
 df_folders <- map_dfr(df_spaces$id, cuf_get_folders)
 df_folders
 
-df_lists <- map_dfr(df_folders$id, cuf_get_lists)
+df_lists <- map_dfr(df_folders$id, cuf_get_lists, .progress = TRUE)
 df_lists
 
-tasks_list <- map(df_lists$id, cu_get_tasks, subtasks = TRUE, include_closed = TRUE)
-tasks_list_archived <- map(df_lists$id, cu_get_tasks, archived = TRUE, subtasks = TRUE, include_closed = TRUE)
+tasks_list <- map(df_lists$id, cu_get_tasks, subtasks = TRUE, include_closed = TRUE, .progress = TRUE)
+tasks_list_archived <- map(df_lists$id, cu_get_tasks, archived = TRUE, subtasks = TRUE, include_closed = TRUE, .progress = TRUE)
 
 tasks <-
     c(tasks_list, tasks_list_archived) %>%
@@ -21,11 +21,10 @@ tasks <-
 
 tasks_team <- cu_get_filtered_team_tasks(df_teams$id[[1]])
 
-spec_tasks <- spec_guess(c(tasks, tasks_team$tasks))
-
-# Error in tibblify_impl(x, spec) :
-#   SET_VECTOR_ELT() can only be applied to a 'list', not a 'NULL'
-df_tasks <- tibblify::tibblify(c(tasks, tasks_team$tasks), spec = spec_tasks, unspecified = "inform")
-df_tasks
-
+spec_tasks <- guess_tspec(c(tasks, tasks_team$tasks))
 write_spec(spec_tasks)
+rm(spec_tasks)
+
+pkgload::load_all()
+
+cuf_get_tasks(df_lists$id[[1]])
